85eceba2aa554331e86ce672d357e4461858c64f
charlesgobina <charles.gobina@gmail.com>
2021-06-17 10:34:46 -0700

    Printing without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-write(2,"with proper grammar, but the outcome is a piece of art\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
