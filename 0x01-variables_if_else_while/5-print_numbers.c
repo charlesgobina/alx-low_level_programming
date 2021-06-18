6c1438c511925d3a53ece8d6de8ad7ff85806e72
charlesgobina <charles.gobina@gmail.com>
2021-06-18 09:55:21 -0700

    Prints 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print last digit in random number
+ *
+ * Description: print last digit in random number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 10, i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
