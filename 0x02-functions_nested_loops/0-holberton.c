a35e22390da36b59cc7ef0b101e633ab82daa0e6
charlesgobina <charles.gobina@gmail.com>
2021-06-21 15:54:15 -0700

    Prints out Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - prints holberton
+ *
+ * Description: prints holterton
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Holberton");
+	printf("\n");
+	return (0);
+}
