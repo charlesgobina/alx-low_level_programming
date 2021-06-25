471e962f8770dff5bdc0a4c9dd7d3056f9ea78c9
charlesgobina <charles.gobina@gmail.com>
2021-06-25 10:09:17 -0700

    The FizzBuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - checks if alphabet is upper or lowercase
+ * Description: The fizz buzz program
+ * Return: void
+ */
+
+int main(void)
+{
+
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+		i++;
+	}
+
+	return (0);
+
+
+}
