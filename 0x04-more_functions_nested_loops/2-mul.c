ffb6a832d1b2f9e13f1c7a55b8c7623286c0302a
charlesgobina <charles.gobina@gmail.com>
2021-06-25 05:12:06 -0700

    Returns the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - checks if alphabet is upper or lowercase
+ *
+ * @a: first integer
+ * @b: Second integer
+ * Return: void
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
