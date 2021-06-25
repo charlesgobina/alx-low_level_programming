4667ab29189b814ce4e1b6449fa33e688f67dd61
charlesgobina <charles.gobina@gmail.com>
2021-06-25 12:11:32 -0700

    Prints 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - checks if alphabet is upper or lowercase
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+
+	int a = 0;
+	int b = 0;
+
+	while (b <= 9)
+	{
+		while (a <= 14)
+		{
+			if (a > 9)
+			{
+				_putchar(a / 10 + '0');
+				_putchar(a % 10 + '0');
+				a++;
+			}
+		}
+		_putchar('\n');
+		b++;
+		a = 0;
+
+	}
+
+
+}
